bc08bd7ab5784f291d934d3ec2f50b62cd00f37e
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-09-22 13:20:27 +0200

    TreeHierarchy: loading test-JSON
    adding code based on template for loading json-files from exercise 9
    
    added main, type alias Model, init, treeDecoder, update, view, 2 helpers
    
    inserting local url for test file

--- src/TreeHierarchy.elm
+++ src/TreeHierarchy.elm
@@ -15,3 +15,93 @@ import TypedSvg.Types as ST exposing (AnchorAlignment(..), Length(..), Paint(..)
 import Color
 import TreeLayout
 import Tree exposing (Tree)
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \m -> Sub.none
+        }
+
+type alias Model =
+    { tree : Tree String, errorMsg : String }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { tree = Tree.singleton "", errorMsg = "Loading ..." }
+    , Http.get { url = "../Daten/JSON/XBoxOne_GamesSales_test.json", expect = Http.expectJson GotFlare treeDecoder }
+    )
+
+
+type Msg
+    = GotFlare (Result Http.Error (Tree String))
+
+
+treeDecoder : Json.Decode.Decoder (Tree String)
+treeDecoder =
+    Json.Decode.map2
+        (\name children ->
+            case children of
+                Nothing ->
+                    Tree.tree name []
+
+                Just c ->
+                    Tree.tree name c
+        )
+        (Json.Decode.field "name" Json.Decode.string)
+        (Json.Decode.maybe <|
+            Json.Decode.field "children" <|
+                Json.Decode.list <|
+                    Json.Decode.lazy
+                        (\_ -> treeDecoder)
+        )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotFlare (Ok newTree) ->
+            ( { model | tree = newTree, errorMsg = "No Error" }, Cmd.none )
+
+        GotFlare (Err error) ->
+            ( { model
+                | tree = Tree.singleton ""
+                , errorMsg =
+                    case error of
+                        Http.BadBody newErrorMsg ->
+                            newErrorMsg
+
+                        _ ->
+                            "Some other Error"
+              }
+            , Cmd.none
+            )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ Html.text model.errorMsg
+        , Tree.restructure labelToHtml toListItems model.tree
+        ]
+
+
+--helpers for Tree structure
+labelToHtml : String -> Html msg
+labelToHtml l =
+    Html.text l
+
+
+toListItems : Html msg -> List (Html msg) -> Html msg
+toListItems label children =
+    case children of
+        [] ->
+            Html.li [] [ label ]
+
+        _ ->
+            Html.li []
+                [ label
+                , Html.ul [] children
+                ]
