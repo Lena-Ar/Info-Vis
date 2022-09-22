07cfb23fd0b46008ce75d79b63e9299c242ac5df
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-09-22 12:54:12 +0200

    TreeHierarchy: updating and correcting imports
    adding to TypedSvg, adding Dict, Scale, Statistics
    deleting TreeDiagramm and adding TreeLayout and Tree

--- src/TreeHierarchy.elm
+++ src/TreeHierarchy.elm
@@ -3,12 +3,15 @@ module TreeHierarchy exposing (..)
 import Browser
 import Http
 import Json.Decode
+import Dict
+import Scale
+import Statistics
 import Html exposing (Html, div, text)
+import TypedSvg exposing (circle, g, line, path, rect, style, svg, text_)
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Attributes exposing (stroke, fill, textAnchor, transform, fontFamily, fontSize)
-import TypedSvg.Attributes.InPx exposing (cx, cy, r, x1, x2, y1, y2)
+import TypedSvg.Attributes exposing (d, class, stroke, strokeWidth, fill, textAnchor, transform, fontFamily, fontSize, viewBox)
+import TypedSvg.Attributes.InPx exposing (cx, cy, r, x1, x2, y1, y2, x, y)
 import TypedSvg.Types as ST exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
 import Color
-import TreeDiagram
-import TreeDiagram exposing (TreeLayout, topToBottom)
-import TreeDiagram.Svg
+import TreeLayout
+import Tree exposing (Tree)
