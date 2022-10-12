c1afba0243c6cdc68e47910190f31ed98ecacf39
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-10-12 11:09:12 +0200

    Data: add GameSales from Scatterplot/ParallelPlot

--- src/Data.elm
+++ src/Data.elm
@@ -1,1 +1,12 @@
 module Data exposing (..)
+
+type alias GameSales =
+    { game : String
+    , genre : String
+    , publisher : String
+    , northAmerica : Float
+    , europe : Float
+    , japan : Float
+    , restOfWorld : Float
+    , global : Float
+    }
