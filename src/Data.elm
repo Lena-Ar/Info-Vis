a9845bb5b9028203e40d8a3be907daaa4c574df8
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-10-12 11:40:45 +0200

    Data: add all decoder parts
    add decodeGameSales, csvString_to_data, gameSalesList,
    from Scatterplot/ParallelPlot,
    update imports

--- src/Data.elm
+++ src/Data.elm
@@ -1,5 +1,34 @@
 module Data exposing (..)
 
+import Csv exposing (parse)
+import Csv.Decode exposing (..)
+
+--Decoder
+decodeGameSales : Csv.Decode.Decoder (GameSales -> a) a
+decodeGameSales =
+    Csv.Decode.map GameSales
+        (Csv.Decode.field "Game" Ok
+            |> Csv.Decode.andMap (Csv.Decode.field "Genre" Ok)
+            |> Csv.Decode.andMap (Csv.Decode.field "Publisher" Ok)
+            |> Csv.Decode.andMap (Csv.Decode.field "North America" (String.toFloat >> Result.fromMaybe "error parsing string"))
+            |> Csv.Decode.andMap (Csv.Decode.field "Europe" (String.toFloat >> Result.fromMaybe "error parsing string"))
+            |> Csv.Decode.andMap (Csv.Decode.field "Japan" (String.toFloat >> Result.fromMaybe "error parsing string"))
+            |> Csv.Decode.andMap (Csv.Decode.field "Rest of World" (String.toFloat >> Result.fromMaybe "error parsing string"))
+            |> Csv.Decode.andMap (Csv.Decode.field "Global" (String.toFloat >> Result.fromMaybe "error parsing string"))
+        )
+
+csvString_to_data : String -> List GameSales
+csvString_to_data csvRaw =
+    Csv.parse csvRaw
+        |> Csv.Decode.decodeCsv decodeGameSales
+        |> Result.toMaybe
+        |> Maybe.withDefault []
+
+gamesSalesList :List String -> List GameSales
+gamesSalesList listGame =
+    List.map(\x -> csvString_to_data x) listGame
+        |> List.concat
+
 type alias GameSales =
     { game : String
     , genre : String
