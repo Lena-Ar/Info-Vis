cb6ac5d305f41803dfa0ecec2d9134bee54a345e
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-10-14 08:56:12 +0200

    MainScatterParallel: insert main parts of structure
    add main, subscriptions, init, update from ParallelPlot,
    update imports

--- src/MainScatterParallel.elm
+++ src/MainScatterParallel.elm
@@ -1,1 +1,82 @@
 module MainScatterParallel exposing (..)
+
+import Browser
+import Http
+import Csv exposing (parse)
+import Csv.Decode exposing (..)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Loading
+    , Http.get
+        { url = "https://raw.githubusercontent.com/Lena-Ar/Info-Vis/main/Daten/CSV/XboxOne_GameSales_test.csv"
+        , expect = Http.expectString GotText
+        }
+    )
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success <| { data = Data.gamesSalesList [fullText], genre = "Action", axis1 = .northAmerica, axis2 = .europe, axis3 = .japan, axis4 = .restOfWorld, axis5 = .global, name1 = "North America", name2 = "Europe", name3 = "Japan", name4 = "Rest of World", name5 = "Global" }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+        
+        ChangeGenreType new_genre -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = new_genre, axis1 = a.axis1, axis2 = a.axis2, axis3 = a.axis3, axis4 = a.axis4, axis5 = a.axis5, name1 = a.name1, name2 = a.name2, name3 = a.name3, name4 = a.name4, name5 = a.name5 }, Cmd.none ) 
+                _ ->
+                    ( model, Cmd.none )
+
+        ChangeFirstAxis (new_axis, new_name) -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = a.genre, axis1 = new_axis, axis2 = a.axis2, axis3 = a.axis3, axis4 = a.axis4, axis5 = a.axis5, name1 = new_name, name2 = a.name2, name3 = a.name3, name4 = a.name4, name5 = a.name5 }, Cmd.none ) 
+                _ -> 
+                    ( model, Cmd.none )
+        
+        ChangeSecondAxis (new_axis, new_name) -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = a.genre, axis1 = a.axis1, axis2 = new_axis, axis3 = a.axis3, axis4 = a.axis4, axis5 = a.axis5, name1 = a.name1, name2 = new_name, name3 = a.name3, name4 = a.name4, name5 = a.name5 }, Cmd.none ) 
+                _ -> 
+                    ( model, Cmd.none )
+        
+        ChangeThirdAxis (new_axis, new_name) -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = a.genre, axis1 = a.axis1, axis2 = a.axis2, axis3 = new_axis, axis4 = a.axis4, axis5 = a.axis5, name1 = a.name1, name2 = a.name2, name3 = new_name, name4 = a.name4, name5 = a.name5 }, Cmd.none ) 
+                _ -> 
+                    ( model, Cmd.none )
+        
+        ChangeFourthAxis (new_axis, new_name) -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = a.genre, axis1 = a.axis1, axis2 = a.axis2, axis3 = a.axis3, axis4 = new_axis, axis5 = a.axis5, name1 = a.name1, name2 = a.name2, name3 = a.name3, name4 = new_name, name5 = a.name5 }, Cmd.none ) 
+                _ -> 
+                    ( model, Cmd.none )
+        
+        ChangeFifthAxis (new_axis, new_name) -> 
+            case model of
+                Success a ->
+                    (Success <| { data = a.data, genre = a.genre, axis1 = a.axis1, axis2 = a.axis2, axis3 = a.axis3, axis4 = a.axis4, axis5 = new_axis, name1 = a.name1, name2 = a.name2, name3 = a.name3, name4 = a.name4, name5 = new_name }, Cmd.none ) 
+                _ -> 
+                    ( model, Cmd.none )
