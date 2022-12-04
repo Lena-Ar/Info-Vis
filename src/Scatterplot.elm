99912c43b7309f80bd02a0b67a58fc556c419698
Lena-Ar <112771597+Lena-Ar@users.noreply.github.com>
2022-12-04 15:45:03 +0100

    Scatterplot: insert useful comments
    inserting useful comments for code explanations,
    deleting code previously put in comments

--- src/Scatterplot.elm
+++ src/Scatterplot.elm
@@ -16,174 +16,27 @@ import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, x1, x2, y
 import TypedSvg.Core exposing (Svg)
 import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
 import Data exposing (Point, XyData, RegionType(..), GameSales)
-{--
-main : Program () Model Msg
-main =
-    Browser.element
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
-
-type Msg
-    = GotText (Result Http.Error String)
-    | ChangeGenreType String
-    | ChangeRegionX RegionType
-    | ChangeRegionY RegionType
 
 
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none--}
+-- mapping and filtering for Null-values --
+
+-- two helper functions to work around lacking Maybe.map6 --
+-- the same as in ParallelPlot --
+ 
+-- idea based on https://package.elm-lang.org/packages/elm/core/latest/Maybe#map2 --
 {--
-daten : List String
-daten =
-    [ "XBoxOne_GameSales_test.csv" ]
---}{--
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Loading
-    , Http.get
-        { url = "https://raw.githubusercontent.com/Lena-Ar/Info-Vis/main/Daten/CSV/XboxOne_GameSales_test.csv"
-        , expect = Http.expectString GotText
-        }
-    )--}
-{--
-loadingGameSales : (Result Http.Error String -> Msg) -> Cmd Msg
-loadingGameSales gotText = 
-    daten
-        |> List.map
-            (\d ->
-                Http.get
-                    { url = "https://raw.githubusercontent.com/Lena-Ar/Info-Vis/main/Daten/CSV/" ++ d
-                    , expect = Http.expectString gotText
-                    }
-            )
-        |> Cmd.batch
---}{--
-type alias GameSales =
-    { game : String
-    , genre : String
-    , publisher : String
-    , northAmerica : Float
-    , europe : Float
-    , japan : Float
-    , restOfWorld : Float
-    , global : Float
-    }
---}{--
-type Model
-  = Error
-  | Loading
-  | Success 
-    { data: List Data.GameSales
-    , genre: String
-    , xaxis: RegionType
-    , yaxis: RegionType
-    --, genre: GenreType
-    }--}
-{--
-type GenreType
-    = Action
-    | Action_Adventure
-    | Adventure
-    | Fighting
-    | Misc
-    | MMO
-    | Music
-    | Platform
-    | Puzzle
-    | Racing
-    | Role_Playing
-    | Shooter
-    | Simulation
-    | Sports
-    | Strategy
+    helper to help piping/applying Maybe.map2 to helpMapBig
 --}
-
---maybe error bc of field from decoding -> maybe change csv?
-{--
-type RegionType
-    = NorthAmerica
-    | Europe
-    | Japan
-    | RestOfWorld
-    | Global
---}
-{--
-type alias Point =
-    { pointGame : String
-    , pointNorthAmerica : Float
-    , pointEurope : Float
-    , pointJapan : Float
-    , pointRestOfWorld : Float
-    , pointGlobal : Float
-    }
-
-
-type alias XyData =
-    { xDescription : String
-    , yDescription : String
-    , data : List Point
-    }
-    --}
-{--
---Decoder
-decodeGameSales : Csv.Decode.Decoder (Data.GameSales -> a) a
-decodeGameSales =
-    Csv.Decode.map Data.GameSales
-        (Csv.Decode.field "Game" Ok
-            |> Csv.Decode.andMap (Csv.Decode.field "Genre" Ok)
-            |> Csv.Decode.andMap (Csv.Decode.field "Publisher" Ok)
-            |> Csv.Decode.andMap (Csv.Decode.field "North America" (String.toFloat >> Result.fromMaybe "error parsing string"))
-            |> Csv.Decode.andMap (Csv.Decode.field "Europe" (String.toFloat >> Result.fromMaybe "error parsing string"))
-            |> Csv.Decode.andMap (Csv.Decode.field "Japan" (String.toFloat >> Result.fromMaybe "error parsing string"))
-            |> Csv.Decode.andMap (Csv.Decode.field "Rest of World" (String.toFloat >> Result.fromMaybe "error parsing string"))
-            |> Csv.Decode.andMap (Csv.Decode.field "Global" (String.toFloat >> Result.fromMaybe "error parsing string"))
-        )
-
-csvString_to_data : String -> List Data.GameSales
-csvString_to_data csvRaw =
-    Csv.parse csvRaw
-        |> Csv.Decode.decodeCsv decodeGameSales
-        |> Result.toMaybe
-        |> Maybe.withDefault []
-
-gamesSalesList :List String -> List Data.GameSales
-gamesSalesList listGame =
-    List.map(\x -> csvString_to_data x) listGame
-        |> List.concat
---}
---mapping games to point---
---helpers for mapping because of lacking Maybe.map6--
-
-{-- 
-based on https://package.elm-lang.org/packages/elm/core/latest/Maybe#map2
-; helper which helps with piping (which is a function)/applying map2 to be bigger one following
---}
-
 map2pipe : Maybe a -> Maybe ( a -> b) -> Maybe b
 map2pipe = 
     Maybe.map2 (|>)
 
-{-- first try: basically just did map5 but without the helper map5
-; instead piping; tried with too many arguments; not thought through 
-helpMap : (a -> b -> c -> d -> e -> f) -> Maybe a -> Maybe b -> Maybe c -> Maybe d -> Maybe e -> Maybe f
-helpMap apply m1 m2 m3 m4 m5 m6 = 
-    Just apply
-        |> m1
-        |> m2
-        |> m3
-        |> m4
-        |> m5
-        |> m6
---}
+
+-- idea based on https://package.elm-lang.org/packages/elm/core/latest/Maybe#map5 but with one more to transform --
 {-- 
-now: based on https://package.elm-lang.org/packages/elm/core/latest/Maybe#map5 but with one more to transform
-; therefore map2pipe needed to handle one more than map5 would do
-; applying map2pipe to transform all like a map6 would do if it existed
-; with helper of map2 in map2pipe 
+    therefore map2pipe needed to handle one more than map5 would do
+    applying map2pipe to transform all like a map6 would do if it existed
+    with helper of map2 in map2pipe 
 --}
 helpMapBig : (nam -> na -> eu -> ja -> row -> gl) -> Maybe nam -> Maybe na -> Maybe eu -> Maybe ja -> Maybe row -> Maybe gl
 helpMapBig apply a b c d e = 
@@ -194,29 +47,9 @@ helpMapBig apply a b c d e =
         |> map2pipe d
         |> map2pipe e
 
--- based on https://ellie-app.com/hhZMpcRnTwFa1 (my code for exercise 1) --
---maybe need changes here--
-{--
-assignment : Data.GameSales -> Maybe Point
-assignment game =
-    helpMapBig
-        (\northAmerica europe japan restOfWorld global ->
-            Point
-                (game.game ++ " , " ++  game.publisher ++ " ")
-                (northAmerica)
-                (europe)
-                (japan)
-                (restOfWorld)
-                (global)
-        )
-        (Just game.northAmerica)
-        (Just game.europe)
-        (Just game.japan)
-        (Just game.restOfWorld)
-        (Just game.global)--}
 
--- filtering &assigning games --
--- based on https://ellie-app.com/hhZMpcRnTwFa1 (my code for exercise 1) --
+-- filtering and assigning games with help of the two above helpers --
+-- based on code for exercise 1 (https://ellie-app.com/hhZMpcRnTwFa1) --
 filterAndReduceGames : List GameSales -> XyData
 filterAndReduceGames games =
     let
@@ -241,293 +74,10 @@ filterAndReduceGames games =
             List.filterMap assignment games
     in
     XyData "North America" "Europe" filter
+      
 
---filter for genre--
-{--
-filterGenre : List Data.GameSales -> String -> List Data.GameSales
-filterGenre allGames genretype =
-    List.filter (\c -> c.genre == genretype) allGames
-    --}
-{--
---genre ändern
-genreTypeToString : GenreType -> String
-genreTypeToString genreType =
-    case genreType of
-        Action -> 
-            "Action"
-
-        Action_Adventure ->
-            "Action-Adventure"
-
-        Adventure ->
-            "Adventure"
-
-        Fighting ->
-            "Fighting"
-
-        Misc ->
-            "Misc"
-
-        MMO ->
-            "MMO"
-
-        Music ->
-            "Music"
-        
-        Platform ->
-            "Platform"
-        
-        Puzzle ->
-            "Puzzle"
-        
-        Racing ->
-            "Racing"
-        
-        Role_Playing ->
-            "Role-Playing"
-        
-        Shooter ->
-            "Shooter"
-        
-        Simulation ->
-            "Simulation"
-        
-        Sports ->
-            "Sports"
-        
-        Strategy ->
-            "Strategy"
-
-stringToGenreType : String -> GenreType
-stringToGenreType stringGenreType =
-    if stringGenreType == "Action" then
-        Action
-
-    else if stringGenreType == "Action-Adventure" then
-        Action_Adventure
-
-    else if stringGenreType == "Adventure" then
-        Adventure
-
-    else if stringGenreType == "Fighting" then
-        Fighting
-
-    else if stringGenreType == "Misc" then
-        Misc
-
-    else if stringGenreType == "MMO" then
-        MMO
-
-    else if stringGenreType == "Music" then
-        Music
-
-    else if stringGenreType == "Platform" then
-        Platform
-
-    else if stringGenreType == "Puzzle" then
-        Puzzle
-    
-    else if stringGenreType == "Racing" then
-        Racing
-    
-    else if stringGenreType == "Role-Playing" then
-        Role_Playing
-    
-    else if stringGenreType == "Shooter" then
-        Shooter
-    
-    else if stringGenreType == "Simulation" then
-        Simulation
-
-    else if stringGenreType == "Sports" then
-        Sports
-    
-    else
-        Strategy
---}
-{--
---attributType/region
-regionTypeToString : RegionType -> String
-regionTypeToString regionType =
-    case regionType of
-        NorthAmerica -> 
-            "North America"
-
-        Europe ->
-            "Europe"
-
-        Japan ->
-            "Japan"
-
-        RestOfWorld ->
-            "Rest of world"
-
-        Global ->
-            "Global"
-
-        
-stringToRegionType : String -> RegionType
-stringToRegionType stringRegionType =
-    if stringRegionType == "North America" then
-        NorthAmerica
-
-    else if stringRegionType == "Europe" then
-        Europe
-
-    else if stringRegionType == "Japan" then
-        Japan
-
-    else if stringRegionType == "Rest of world" then
-        RestOfWorld
-
-    else
-        Global
---}
-{--
-buttonGenreType : Html Msg
-buttonGenreType =
-    Html.select
-        [ onInput ChangeGenreType ]
-        [ Html.option [ value "Action" ] [ Html.text "Action" ]
-        , Html.option [ value "Action-Adventure" ] [ Html.text "Action-Adventure" ]
-        , Html.option [ value "Adventure" ] [ Html.text "Adventure" ]
-        , Html.option [ value "Fighting" ] [ Html.text "Fighting" ]
-        , Html.option [ value "Misc" ] [ Html.text "Misc" ]
-        , Html.option [ value "MMO" ] [ Html.text "MMO" ]
-        , Html.option [ value "Music" ] [ Html.text "Music" ]
-        , Html.option [ value "Platform" ] [ Html.text "Platform" ]
-        , Html.option [ value "Puzzle" ] [ Html.text "Puzzle" ]
-        , Html.option [ value "Racing" ] [ Html.text "Racing" ] 
-        , Html.option [ value "Role-Playing" ] [ Html.text "Role-Playing" ] 
-        , Html.option [ value "Shooter" ] [ Html.text "Shooter" ] 
-        , Html.option [ value "Simulation" ] [ Html.text "Simulation" ] 
-        , Html.option [ value "Sports" ] [ Html.text "Sports" ]
-        , Html.option [ value "Strategy" ] [ Html.text "Strategy" ]
-        ]
---}
-{--
-buttonRegionTypeX : Html Msg
-buttonRegionTypeX =
-    Html.select
-        [ onInput (\rx -> stringToRegionType rx |> ChangeRegionX) ]
-        [ Html.option [ value "North America" ] [ Html.text "North America" ]
-        , Html.option [ value "Europe" ] [ Html.text "Europe" ]
-        , Html.option [ value "Japan" ] [ Html.text "Japan" ]
-        , Html.option [ value "Rest of world" ] [ Html.text "Rest of world" ]
-        , Html.option [ value "Global" ] [ Html.text "Global" ]
-        ]
-
-buttonRegionTypeY : Html Msg
-buttonRegionTypeY =
-    Html.select
-        [ onInput (\ry -> stringToRegionType ry |> ChangeRegionY) ]
-        [ Html.option [ value "North America" ] [ Html.text "North America" ]
-        , Html.option [ value "Europe" ] [ Html.text "Europe" ]
-        , Html.option [ value "Japan" ] [ Html.text "Japan" ]
-        , Html.option [ value "Rest of world" ] [ Html.text "Rest of world" ]
-        , Html.option [ value "Global" ] [ Html.text "Global" ]
-        ]
-
---cases for buttons to be added
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        GotText result ->
-            case result of
-                Ok fullText ->
-                --to be included with regions according to model
-                    ( Success <| { data = Data.gamesSalesList [fullText], genre = "Action", xaxis = NorthAmerica , yaxis = NorthAmerica}, Cmd.none )
-                Err _ ->
-                    (model, Cmd.none)
-
-        ChangeGenreType new_genre -> 
-                --to be included with regions according to model
-            case model of
-                Success a ->
-                    (Success <| { data = a.data, genre = new_genre, xaxis = a.xaxis, yaxis = a.yaxis }, Cmd.none ) 
-                _ ->
-                    ( model, Cmd.none )
-        
-        ChangeRegionX new_regionx -> 
-            case model of
-                Success a -> 
-                    (Success <| { data = a.data, genre = a.genre, xaxis = new_regionx, yaxis = a.yaxis}, Cmd.none ) 
-                _ -> 
-                    ( model, Cmd.none )
-        
-        ChangeRegionY new_regiony ->
-            case model of
-                Success a -> 
-                    (Success <| { data = a.data, genre = a.genre, xaxis = a.xaxis, yaxis = new_regiony}, Cmd.none ) 
-                _ -> 
-                    ( model, Cmd.none )
---}
-{--
---to be adjusted for axisChange/region--
-view : Model -> Html Msg
-view model =
-    case model of
-        Error ->
-            text "Opening the data for sales of games on XBoxOne failed"
-
-        Loading ->
-            text "Loading GameSales data..."
-        
-        Success fullText ->
-            Html.div [Html.Attributes.style "padding" "10px"]
-                [ Html.h1 [Html.Attributes.style "fontSize" "30px"] 
-                    [ Html.text ("Scatterplot of Video Game Sales for XBox One") ]
-                , Html.h2 [Html.Attributes.style "fontSize" "20px"] 
-                    [ Html.text ("This scatterplot shows the sales of video games in millions of units for XBox One sorted by selected genre.") ]
-                , Html.p [Html.Attributes.style "fontSize" "17px"]
-                    [Html.text ("Hint: The x-axis and y-axis can be adjusted to your needs by seleceting the needed regions for each axis with the buttons below.")]
-                , Html.p [Html.Attributes.style "fontSize" "15px"]
-                    [ Html.text ("Number of all games across all genres: " ++ String.fromInt number_games)
-                    , Html.br [] []
-                    , Html.text ("Number of all games across all genres cleared by potential Null-values: " ++ String.fromInt number_clearedGames)
-                    ]
-                , Html.h4 [Html.Attributes.style "fontSize" "16px"]
-                    [ Html.text ("Please choose the genre you want to display with the button below.") ]
-                , Html.p [Html.Attributes.style "padding" "10px"]
-                    [ buttonGenreType ]
-                , Html.p [Html.Attributes.style "fontSize" "15px"]
-                    [ Html.text ("Number of games in selected genre: " ++ String.fromInt number_filteredGames) ]
-                , Html.h4 [Html.Attributes.style "fontSize" "16px"]
-                    [ Html.text ("Please choose the region you want to display on the xaxis first (on the left) and the one you want to display on the yaxis second (on the right).")]
-                , Html.p [Html.Attributes.style "padding" "10px"]
-                    [ buttonRegionTypeX
-                    , buttonRegionTypeY ]
-                , Html.h2 [Html.Attributes.style "fontSize" "20px"]
-                    [Html.text ("Scatterplot for " ++ fullText.genre ++ " with " ++ (regionTypeToString fullText.xaxis) ++ " as x-axis and " ++ (regionTypeToString fullText.yaxis) ++ " as y-axis.")]
-                , scatterplot cssPoint gameSalesDataCleared valuesX valuesY (regionTypeToString fullText.xaxis) (regionTypeToString fullText.yaxis)
-                ]--}
-                {--
-gameSalesData: List Data.GameSales
-gameSalesData = 
-    fullText.data
-                
-gameSalesDataFiltered = 
-    filterGenre fullText.data fullText.genre
-                
-                
-gameSalesDataNull =
-    filterAndReduceGames (fullText.data)
-            
-number_clearedGames: Int
-number_clearedGames = 
-    List.length gameSalesDataNull.data
-
-number_games: Int
-number_games =
-    List.length gameSalesData
-
-number_filteredGames : Int
-number_filteredGames =
-    List.length gameSalesDataFiltered
-                
-gameSalesDataCleared = 
-    filterAndReduceGames (gameSalesDataFiltered)
-          --}      
+-- needed for free selection of x- and y-axis --
+-- applied with giving data in valuesX and valuesY in case for Scatterplot in MainScatterParallel --
 regionFilter : List GameSales -> RegionType -> List Float
 regionFilter points regionType =
     case regionType of
@@ -545,17 +95,9 @@ regionFilter points regionType =
                     
         Global -> 
             List.map .global points
-{--         
-valuesX : List Float
-valuesX = 
-    regionFilter gameSalesDataFiltered fullText.xaxis
 
 
-valuesY : List Float
-valuesY = 
-    regionFilter gameSalesDataFiltered fullText.yaxis--}
-----------point--------------
-
+-- definition of point for use in scatterplot --
 point : ContinuousScale Float -> ContinuousScale Float -> Point -> (Float, Float) -> Svg msg
 point scaleX scaleY pointLabel xyPoint =
     g
@@ -584,17 +126,17 @@ point scaleX scaleY pointLabel xyPoint =
                     ++ ")" 
             ]
             ]
-        
 
-----Scatterplot--------------------
-------------------------------------
+
+-- Scatterplot to draw plot--
 scatterplot : String -> XyData -> List Float -> List Float -> String -> String -> Svg msg
 scatterplot css model xValues yValues labelX labelY =
     let
+        -- to ensure variability of axes/x- and y-values --
         pointsXY = 
             List.map2 (\x y -> ( x, y )) xValues yValues
 
-        --Abbildungen/Umrechnungen auf SVG
+        -- compute to SVG --
         xScaleLocal : ContinuousScale Float
         xScaleLocal =
             xScale xValues
@@ -622,7 +164,7 @@ scatterplot css model xValues yValues labelX labelY =
             , fontFamily [ "sans-serif" ]
             ]
             []
-        --x-Achse
+        -- x-axis --
         , g
             [ transform [ Translate padding (h - padding) ] ]
             [ xAxis xValues
@@ -636,7 +178,7 @@ scatterplot css model xValues yValues labelX labelY =
                 [ Html.text labelX ]
             ]
 
-        -- y-Achse
+        -- y-axis --
         , g
             [ transform [ Translate padding padding ] ]
             [ yAxis yValues
@@ -650,11 +192,13 @@ scatterplot css model xValues yValues labelX labelY =
                 [ Html.text labelY ]
             ]
 
-        --SVG der Points
+        -- points --
         , g [ transform [ Translate padding padding ] ]
             (List.map2 (point xScaleLocal yScaleLocal) model.data pointsXY)
         ]
 
+
+-- globally defined CSS --
 cssPoint : String
 cssPoint = 
     """
@@ -664,10 +208,8 @@ cssPoint =
         .point:hover text { display: inline; stroke: rgba(255, 255, 255, 1); stroke-width: 0.03; fill: rgb(75, 128, 36, 0.8)}
     """ 
 
----------------------------------------------
-------------general settings for scatterplot----------
 
-
+-- general definitions for scatterplot--
 w : Float
 w =
     800
